8c28a435707941736104fb1e62b4b8cb6101b545
NeAndrew <65078011+Andrey1233-maker@users.noreply.github.com>
2022-04-12 21:52:02 +0300

    labaWithBadInterface

--- Lab1/ConsoleInterface.cs
+++ Lab1/ConsoleInterface.cs
@@ -8,5 +8,27 @@ namespace Lab1
 {
     internal class ConsoleInterface
     {
+
+        class InterfaceConsts
+        {
+            static public string HELLO_STRING = "Это 1 лабораторная работа 2 вариант";
+
+            static public string CREATE_TELEPHONE_COMMAND = "create";
+            static public string CREATE_CONNECTION_COMMAND = "connection";
+            static public string DROP_CONNECTION_COMMAND = "drop";
+            static public string REMOVE_TELEPHONE_COMMAND = "remove";
+            static public string END_COMMAND = "end";
+        }
+
+        public void main()
+        {
+            bool isWork = true;
+
+            while (isWork)
+            {
+
+            }
+        }
+
     }
 }
